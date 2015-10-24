bd5a907be03f9b6aad5e5e717fb26bba17df1f6b
riamirchandani <riamirchandani94@gmail.com>
2015-10-24 16:57:51 +0530

    makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# makeCachedMatrix is a function that returns a list of functions
+# Its puspose is to store a martix and a cached value of the inverse of the 
+# matrix. Contains the following functions:
+# * setMatrix      set the value of a matrix
+# * getMatrix      get the value of a matrix
+# * cachedInverse  get the cahced value (inverse of the matrix)
+# * getInverse     get the cahced value (inverse of the matrix)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # holds the cached value
+  # NULL if nothing is cached
+  # initially it is set to NULL
+  c<-NULL
+  
+  # store a matrix
+  setMatrix <- function(new_val) {
+    x <<- new_val
+    # since the matrix is assigned a new value, delete the cached
+    c <<- NULL
+  }
+  
+  # returns the stored matrix
+  getMatrix <- function() x
+  
+  # c the given argument 
+  cachedInverse <- function(solve) {
+    c <<- solve
+  }
+  
+  # get the cached value
+  getInverse <- function() c
+  
+  # return a list. Each named element of the list is a function
+  list(setMatrix = setMatrix, getMatrix = getMatrix, cachedInverse = cachedInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+# The following function calculates the inverse of a matrix which will be using func. makeCachedMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # get the cached value
+  inverse <- x$getInverse()
+  # if a cached value exists then return it
+  if(!is.null(inverse)) {
+    message("Getting the cached data. Wait")
+    return(inverse)
+  }
+  # otherwise get the matrix, caclulate the inverse and store it in cache
+  data <- x$getMatrix()
+  inverse <- solve(data)
+  x$cachedInverse(inverse)
+  
+  # return the inverse
+  inverse
 }
